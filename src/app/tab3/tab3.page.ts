54d6e42404ded75bda5967859de8106c49f60234
juan.roncancio <sbm029@gmail.com>
2024-11-18 19:00:28 -0500

    Ajustando la vista de reportes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
 
   constructor() {}
@@ -15,4 +16,32 @@ export class Tab3Page {
     frecuencia: 'Diario',
   };
 
+    resultados: any[] = []; //resultados, falta agregar lógica
+
+ // acá debemos ajutar la entrada de datos, ya sea por BD externa o local
+  animales = [
+    { nombre: 'Max', raza: 'Labrador', alimentacion: 'Croquetas' },
+    { nombre: 'Mimi', raza: 'Persa', alimentacion: 'Atún' },
+    { nombre: 'Rocky', raza: 'Golden Retriever', alimentacion: 'Barf' },
+    { nombre: 'Luna', raza: 'Siamés', alimentacion: 'Alimento seco' },
+  ];
+
+  generarReporte() {
+    if (this.reporte.nombre) {
+      this.resultados = this.animales.filter((animal) =>
+        animal.nombre.toLowerCase().includes(this.reporte.nombre.toLowerCase())
+      );
+    } else {
+      this.resultados = this.animales;
+    }
+
+    if (this.resultados.length === 0) {
+      alert('No se encontraron resultados.');
+    }
+  }
+  
+
 }
+
+
+
